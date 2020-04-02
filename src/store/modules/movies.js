2d4c9887df2f8380e2c413d19ad7c7be75068a37
Ivan Neverovsky <ivan@neverovsky.com.ua>
2020-04-02 16:57:44 +0300

    interceptors

--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -10,8 +10,6 @@ const moviesStore = {
       console.log(context);
       const responce = await axios.get("/", {
         params: {
-          apiKey: process.env.VUE_APP_API_KEY,
-          plot: "full",
           i: "tt3896198"
         }
       });
